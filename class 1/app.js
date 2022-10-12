7fc45388097da9175c1b37d07636cbce1a4d07ad
Sharjeel-1999 <67510731+Sharjeel-1999@users.noreply.github.com>
2022-10-12 21:44:42 +0500

    class-1

---
+++ class 1/app.js
@@ -0,0 +1,57 @@
+// // 1. ways to print in js 
+// console.log("Hello World");
+// alert("Hello ");
+
+// document.write("<br/><b>his is javascript code</b>") 
+
+
+// //  2.Js console api
+// console.log("hello world");
+// // console.write("hello world");
+// console.warn("hello world");
+// console.error("hello world");
+// console.assert("hello world");
+
+
+// 3. js varaiables
+
+// var num1=30;
+// var num2 =30;
+// console.log(num1+num2);
+
+
+// 4.data types in js
+//  numbers  (1,2,3,4,5,6,)
+// Boolean (true/false)
+// strings("hello")
+// objescts {
+//     heelo:"aha"
+// }
+
+// at a very high level, there are two types of data types 
+
+// // 1 primitive data types 
+// six types of primitive data types
+// 1 Number
+// 2 undefined
+// 3 null 
+// 4 string 
+// 5 boolean 
+// 6 symbol
+
+
+// 2 reference data types
+// two types of  reference data ty1pes
+
+// 1 arrays 
+//  2 objects
+
+//operators in js
+//arithmatic operators
+// + - / * 
+
+
+
+
+
+
